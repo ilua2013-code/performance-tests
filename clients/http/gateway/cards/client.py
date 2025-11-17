51fd208847e2a6b9a76f34d3712a7dc7791cc775
ilua2013 <ilua.2013@inbox.ru>
2025-11-18 00:12:26 +0300

    Практикуемся в использовании HTTP API клиентов

--- clients/http/gateway/cards/client.py
+++ clients/http/gateway/cards/client.py
@@ -1,16 +1,52 @@
 from typing import TypedDict
-
+from clients.http.gateway.client import build_gateway_http_client
 from httpx import Response
 
 from clients.http.client import HTTPClient
 
 
-class CardsGatewayRequestDict(TypedDict):
+
+class CardDict(TypedDict):
     """
-    Структура данных для создания карты.
+    Описание структуры карты.
+    """
+    id: str
+    pin: str
+    cvv: str
+    type: str
+    status: str
+    accountId: str
+    cardNumber: str
+    cardHolder: str
+    expiryDate: str
+    paymentSystem: str
+
+
+class IssueVirtualCardRequestDict(TypedDict):
+    """
+    Структура данных для выпуска виртуальной карты.
     """
     userId: str
     accountId: str
+
+# Добавили описание структуры ответа выпуска виртуальной карты
+class IssueVirtualCardResponseDict(TypedDict):
+    """
+    Описание структуры ответа выпуска виртуальной карты.
+    """
+    card: CardDict
+
+class IssuePhysicalCardRequestDict(IssueVirtualCardRequestDict):
+    """
+    Структура данных для выпуска физической карты.
+    """
+    pass
+
+class IssuePhysicalCardResponseDict(TypedDict):
+    """
+    Описание структуры ответа выпуска физической карты.
+    """
+    card: CardDict
 
 
 class CardsGatewayHTTPClient(HTTPClient):
@@ -18,7 +54,7 @@ class CardsGatewayHTTPClient(HTTPClient):
     Клиент для взаимодействия с /api/v1/cards/issue-virtual-card.
     """
 
-    def issue_virtual_card_api(self, request: CardsGatewayRequestDict) -> Response:
+    def issue_virtual_card_api(self, request: IssueVirtualCardRequestDict) -> Response:
         """
         Создает виртуальную карту для пользователя.
 
@@ -27,7 +63,7 @@ class CardsGatewayHTTPClient(HTTPClient):
         """
         return self.post("/api/v1/cards/issue-virtual-card", json=request)
 
-    def issue_physical_card_api(self, request: CardsGatewayRequestDict) -> Response:
+    def issue_physical_card_api(self, request: IssuePhysicalCardRequestDict) -> Response:
         """
         Создает физическую карту для пользователя.
 
@@ -35,3 +71,23 @@ class CardsGatewayHTTPClient(HTTPClient):
         :return: Ответ от сервера с данными созданной физической карты.
         """
         return self.post("/api/v1/cards/issue-physical-card", json=request)
+    
+    # Добавили новый метод
+    def issue_virtual_card(self, user_id: str, account_id: str) -> IssueVirtualCardResponseDict:
+        request = IssueVirtualCardRequestDict(userId=user_id, accountId=account_id)
+        response = self.issue_virtual_card_api(request)
+        return response.json()
+
+    # Добавили новый метод
+    def issue_physical_card(self, user_id: str, account_id: str) -> IssuePhysicalCardResponseDict:
+        request = IssuePhysicalCardRequestDict(userId=user_id, accountId=account_id)
+        response = self.issue_physical_card_api(request)
+        return response.json()
+
+def build_cards_gateway_http_client() -> CardsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр CardsGatewayHTTPClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию CardsGatewayHTTPClient.
+    """
+    return CardsGatewayHTTPClient(client=build_gateway_http_client())
---
+++ clients/http/gateway/documents/client.py
@@ -0,0 +1,35 @@
+from httpx import Response
+from clients.http.gateway.client import build_gateway_http_client
+from clients.http.client import HTTPClient
+
+
+class DocumentsGatewayHTTPClient(HTTPClient):
+    """
+    Клиент для взаимодействия с /api/v1/documents сервиса http-gateway.
+    """
+
+    def get_tariff_document_api(self, account_id: str) -> Response:
+        """
+        Получить тарифа по счету.
+
+        :param account_id: Идентификатор счета.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.get(f"/api/v1/documents/tariff-document/{account_id}")
+
+    def get_contract_document_api(self, account_id: str) -> Response:
+        """
+        Получить контракта по счету.
+
+        :param account_id: Идентификатор счета.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.get(f"/api/v1/documents/contract-document/{account_id}")
+    
+def build_documents_gateway_http_client() -> DocumentsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр DocumentsGatewayHTTPClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию DocumentsGatewayHTTPClient.
+    """
+    return DocumentsGatewayHTTPClient(client=build_gateway_http_client())
