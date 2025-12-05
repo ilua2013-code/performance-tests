254ddd3b218f4e118300502f969f1fbec5490e1a
ilua2013 <ilua.2013@inbox.ru>
2025-12-05 17:28:32 +0300

    Практика генерации данных с помощью сидингов. Практика: реализация сидинга для сценария выпуска новой виртуальной карты

--- seeds/dumps.py
+++ seeds/dumps.py
@@ -8,12 +8,12 @@ def save_seeds_result(result: SeedsResult, scenario: str):
     if not os.path.exists("dumps"):
         os.mkdir("dumps")
 
-    with open(f"./dumps/{scenario}.seeds.json", "w+", encoding="utf-8") as file:
+    with open(f"./dumps/{scenario}_seeds.json", "w+", encoding="utf-8") as file:
         file.write(result.model_dump_json(indent=2))
 
 
 def load_seeds_result(scenario: str) -> SeedsResult:
     """Загружает результат Seeds из JSON файла"""
-    with open(f"./dumps/{scenario}.seeds.json", "r", encoding="utf-8") as file:
+    with open(f"./dumps/{scenario}_seeds.json", "r", encoding="utf-8") as file:
         return SeedsResult.model_validate_json(file.read())
     
