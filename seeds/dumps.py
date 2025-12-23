7f41f573a3330e02f0d182e98a632c70e318ce92
ilua2013 <ilua.2013@inbox.ru>
2025-12-23 21:25:04 +0300

    Добавляем логирование для нагрузочных тестов. Практика: работа с логированием в нагрузочных тестах

--- seeds/dumps.py
+++ seeds/dumps.py
@@ -1,19 +1,25 @@
 from seeds.schema.result import SeedsResult
 import os
+from tools.logger import get_logger
 
+                  
+logger = get_logger("SEEDS_DUMPS")
 
 
 def save_seeds_result(result: SeedsResult, scenario: str):
     """Сохраняет результат Seeds в JSON файл"""
+    seeds_file = f"./dumps/{scenario}_seeds.json"
     if not os.path.exists("dumps"):
         os.mkdir("dumps")
-
-    with open(f"./dumps/{scenario}_seeds.json", "w+", encoding="utf-8") as file:
+    with open(seeds_file, "w+", encoding="utf-8") as file:
         file.write(result.model_dump_json(indent=2))
-
+    logger.debug(f"Seeding result saved to file: {seeds_file}")
 
 def load_seeds_result(scenario: str) -> SeedsResult:
     """Загружает результат Seeds из JSON файла"""
-    with open(f"./dumps/{scenario}_seeds.json", "r", encoding="utf-8") as file:
-        return SeedsResult.model_validate_json(file.read())
-    
+    seeds_file = f"./dumps/{scenario}_seeds.json"
+    with open(seeds_file, "r", encoding="utf-8") as file:
+        result = SeedsResult.model_validate_json(file.read())
+        logger.debug(f"Seeding result loaded from file: {seeds_file}")
+        
+        return result
